dccc96828eccf8a329b9c72dd5818dff3feb676f
thi44416 <thirumal.janakiraman@ipt.fraunhofer.de>
2022-06-09 17:02:56 +0000

    basic setup

---
+++ Backend/config.py
@@ -0,0 +1,8 @@
+#Common Configurations
+hostName = "0.0.0.0"
+
+#Ports
+portForUpload = 4000 
+
+#Upload Service Configurations
+datasetFolderLocation = "C:\\Thiru workspace\\Fraunhofer IPT\\WS\\Backend\\Datasets"
